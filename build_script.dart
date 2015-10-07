2229c932313dbce4c5f6a4cf8066713db24913b8
Xavier Hainaux <xavier.hainaux@gaming1.com>
2015-10-07 15:58:10 +0200

    pub get all

--- build_script.dart
+++ build_script.dart
@@ -1,5 +1,52 @@
 
+import 'dart:io';
 
-main() {
+main() async {
+  pubGetAll();
   print('build script');
+}
+
+_runProcess(String processName, List args) {
+  return Process.start(processName, args).then((Process process) {
+    stdout.addStream(process.stdout);
+    stderr.addStream(process.stderr);
+
+    return process.exitCode;
+  });
+}
+
+pubGetAll() {
+  visitDirectory(Directory.current);
+}
+
+visitDirectory(Directory directory) {
+  Directory.current = directory;
+
+  if (hasPubspec) {
+    runPubGet();
+  } else {
+    for (Directory subDirectory
+    in directory.listSync().where((f) => f is Directory)) {
+      visitDirectory(subDirectory);
+    }
+  }
+}
+
+bool get hasPubspec => new File('pubspec.yaml').existsSync();
+
+runPubGet() {
+  print(Directory.current.path);
+  ProcessResult result = Process.runSync(pubPath, ['get']);
+  if (result.stderr != null && result.stderr.isNotEmpty) {
+    print(result.stderr);
+  }
+  print(result.stdout);
+}
+
+String get pubPath {
+  if (Platform.operatingSystem == 'windows') {
+    return 'pub.bat';
+  } else {
+    return 'pub';
+  }
 }
