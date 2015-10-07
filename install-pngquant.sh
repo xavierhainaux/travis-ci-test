aea0521f4da0949317ad0e3ca979562dc1a1af38
Xavier Hainaux <xavier.hainaux@gaming1.com>
2015-10-07 17:49:50 +0200

    pub get all

--- install-pngquant.sh
+++ install-pngquant.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 if [ ! -d "$HOME/pngquant/lib" ]; then
-  wget https://github.com/pornel/pngquant/archive/2.5.0.tar.gz
-  tar -xzvf pngquant
+  wget https://github.com/pornel/pngquant/archive/2.5.0.tar.gz -O pngquant.tar.gz
+  tar -xzvf pngquant.tar.gz
   cd pngquant && make && make install
 else
   echo 'Using cached directory.';
 fi
+export PATH=$PATH:pngquant
