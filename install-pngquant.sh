9015bb0c87f894e1ae7814cd4276afbe3f13ec57
Xavier Hainaux <xavier.hainaux@produweb.be>
2015-10-07 19:57:09 +0200

    update travis

--- install-pngquant.sh
+++ install-pngquant.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 if [ ! -d "pngquant-2.5.0/lib" ]; then
-  wget https://github.com/pornel/pngquant/archive/2.5.0.tar.gz -O pngquant.tar.gz
-  tar -xzvf pngquant.tar.gz
-  cd pngquant-2.5.0 && ./configure --prefix=$HOME/pngquant-2.5.0 && make install
+  echo 'TODO a remettre ici';
 else
   echo 'Using cached directory.';
 fi
+wget https://github.com/pornel/pngquant/archive/2.5.0.tar.gz -O pngquant.tar.gz
+tar -xzvf pngquant.tar.gz
+cd pngquant-2.5.0 && ./configure --prefix=$HOME/pngquant-2.5.0 && make install
 export PATH=$PATH:$HOME/pngquant-2.5.0/bin
