38899ca1453df4d206a38634d9a9101beb833576
Xavier Hainaux <xavier.hainaux@produweb.be>
2015-10-07 19:04:26 +0200

    update travis

--- install-pngquant.sh
+++ install-pngquant.sh
@@ -8,4 +8,4 @@ fi
 wget https://github.com/pornel/pngquant/archive/2.5.0.tar.gz -O pngquant.tar.gz
 tar -xzvf pngquant.tar.gz
 cd pngquant-2.5.0 && ./configure --prefix=$HOME/pngquant-2.5.0 && make && make install
-export PATH=$PATH:pngquant-2.5.0
+export PATH=$PATH:pngquant-2.5.0/bin
