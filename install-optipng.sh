a6f7c77beb203fe8ef0593d088317639e657e5d2
Xavier Hainaux <xavier.hainaux@produweb.be>
2015-10-08 14:18:25 +0200

    update travis

--- install-optipng.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-if [ ! -d "optipng-0.7.5" ]; then
-  wget http://prdownloads.sourceforge.net/optipng/optipng-0.7.5.tar.gz
-  tar -xzvf optipng-0.7.5.tar.gz
-  cd optipng-0.7.5 && make
-else
-  echo 'Using cached directory.';
-fi
