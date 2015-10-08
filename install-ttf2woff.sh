7f66216cd4789a89302c075b118833b3b6119482
Xavier Hainaux <xavier.hainaux@produweb.be>
2015-10-08 08:30:27 +0200

    update travis

--- install-ttf2woff.sh
+++ install-ttf2woff.sh
@@ -3,7 +3,7 @@ set -e
 if [ ! -d "ttf2woff-0.12" ]; then
   wget http://wizard.ae.krakow.pl/~jb/ttf2woff/ttf2woff-0.12.tar.gz -O ttf2woff.tar.gz
   tar -xzvf ttf2woff.tar.gz
-  cd ttf2woff-0.12 && make install
+  cd ttf2woff-0.12 && make
 else
   echo 'Using cached directory.';
 fi
