0457bb9f48c4c35714202a29f0a3ecc785030338
Xavier Hainaux <xavier.hainaux@gaming1.com>
2015-11-03 10:56:50 +0100

    woff2

--- install-woff2.sh
+++ install-woff2.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 if [ ! -d "woff2" ]; then
-  git clone --recursive https://github.com/google/woff2.git
+  git clone --recursive https://github.com/xavierhainaux/woff2.git
   cd woff2
-  ./configure --enable-cxx11 && make clean all
+  make clean all
 else
   echo 'Using cached directory.';
 fi
