9bf029694f5ca6062d5f9ff3c2df325436c1f1bd
Xavier Hainaux <xavier.hainaux@gaming1.com>
2016-03-29 12:35:08 +0200

    Essayer d'installer dartium

---
+++ install-dartium.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e -x
+
+DART_CHANNEL=$1
+VERSION=$2
+ARCH=$3
+
+AVAILABLE_DART_VERSION=$(curl "https://storage.googleapis.com/dart-archive/channels/${DART_CHANNEL}/release/${VERSION}/VERSION" | python -c \
+    'import sys, json; print(json.loads(sys.stdin.read())["version"])')
+
+echo Fetch Dart channel: ${DART_CHANNEL}
+
+URL_PREFIX=https://storage.googleapis.com/dart-archive/channels/${DART_CHANNEL}/release/${VERSION}
+DARTIUM_URL="$URL_PREFIX/dartium/dartium-$ARCH-release.zip"
+
+download_and_unzip() {
+  ZIPFILE=${1/*\//}
+  curl -O -L $1 && unzip -q $ZIPFILE && rm $ZIPFILE
+}
+
+download_and_unzip $DARTIUM_URL
+
+echo Fetched new dart version $(<dart-sdk/version)
+
+if [[ -n $DARTIUM_URL ]]; then
+  mv dartium-* chromium
+fi
