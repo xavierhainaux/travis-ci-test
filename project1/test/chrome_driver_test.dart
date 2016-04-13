a02962d156132b847517ad65ace86b6e5e8a06a7
Xavier Hainaux <xavier.hainaux@gaming1.com>
2016-04-13 16:55:11 +0200

    double idea

--- project1/test/chrome_driver_test.dart
+++ project1/test/chrome_driver_test.dart
@@ -28,16 +28,14 @@ main() {
   });
 
   test('Start webdriver and take capture with chrome', () async {
-    Process chromeDriver = await _startChromeDriver(4444);
-
-    await _startChromeDriver(4444);
+    Process chromeDriver = await _startChromeDriver(4446);
 
     Map capabilities = Capabilities.chrome;
     capabilities['chromeOptions'] = {
       'binary': Platform.environment['CHROME_BIN']
     };
 
-    Uri wdUri = Uri.parse('http://localhost:4444/wd/hub/');
+    Uri wdUri = Uri.parse('http://localhost:4446/wd/hub/');
     WebDriver webDriver = await createDriver(uri: wdUri, desired: capabilities);
 
 
